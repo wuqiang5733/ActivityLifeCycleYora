a2b857658a0642efe3c883be72079ea53b7c2f00
xiaoqianghou <2662651103@qq.com>
2017-01-04 17:18:34 +0800

    添加了FLAG

--- app/src/main/java/org/dalol/activitylifecycleyora/MainActivity.java
+++ app/src/main/java/org/dalol/activitylifecycleyora/MainActivity.java
@@ -43,7 +43,17 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         button.setOnClickListener(this);
         seperateButton.setOnClickListener(this);
 //        Log.d("测试MainActivity开始","测试");
-
+        findViewById(R.id.activity_main_logout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BaseActivity.IsLoggedIn = false;
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                // 清空 BackStack ，要不，如果是在任意一个界面退出的，一按返回键，会不停的出现登陆界面
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
 
